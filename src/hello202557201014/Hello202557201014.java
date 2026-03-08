98f1341a6f18a95e35edf4324d6e0e99eec6ef73
Hamdan <hamdanabialmujahidin@gmail.com>
2026-03-09 03:59:55 +0700

    add hello world

--- src/hello202557201014/Hello202557201014.java
+++ src/hello202557201014/Hello202557201014.java
@@ -15,6 +15,7 @@ public class Hello202557201014 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
     }
     
 }
